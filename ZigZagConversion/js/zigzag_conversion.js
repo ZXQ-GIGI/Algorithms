fda191568533a7f1a775911f2a2b5e4fa1df09df
gigi <snailzhao1995@163.com>
2017-09-13 09:51:08 +0800

    update zigzag algorithms

--- ZigZagConversion/js/zigzag_conversion.js
+++ ZigZagConversion/js/zigzag_conversion.js
@@ -3,33 +3,32 @@
  * @param {number} numRows
  * @return {string}
  */
-var convert = function(s, numRows) {
-    var len = s.length,
-        period_len = (numRows > 1) ? 2 * numRows - 2 : 1,
-        arr = new Array(),
-        str = '';
-    if(len <= numRows){
-        return s;
-    }
-    for(var i = 0; i < len; i++){
-        arr.push(s[i]);
-        if((i % period_len) >= numRows - 1){
-            for(var j = 0; j < numRows - 2; j++){
-                arr.push('*');
-            }
-        }
-    }
-    console.log(arr);
-    for(var i = 0; i < numRows; i++){
-        var index = i;
-        while(index < arr.length){
-            if(arr[index] != '*'){
-                str += arr[index];
-            }
-            index += numRows;
-        }
-    }
-    return str;
-};
+ var convert = function(s, numRows) {
+     var len = s.length,
+         period_len = (numRows > 1) ? 2 * numRows - 2 : 1,
+         str_tmp = '',
+         str = '';
+     if(len <= numRows){
+         return s;
+     }
+     for(var i = 0; i < len; i++){
+         str_tmp += s[i];
+         if((i % period_len) >= numRows - 1){
+             for(var j = 0; j < numRows - 2; j++){
+                 str_tmp += '*';
+             }
+         }
+     }
+     for(var n = 0; n < numRows; n++){
+         var index = n;
+         while(index < str_tmp.length){
+             if(str_tmp[index] != '*'){
+                 str += str_tmp[index];
+             }
+             index += numRows;
+         }
+     }
+     return str;
+ };
 
 console.log(convert('AAqwee',3));
