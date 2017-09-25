c028c8343266eec40b4488211b123f189b523e15
gigi <snailzhao1995@163.com>
2017-09-25 22:24:01 +0800

    add removeduplicates

---
+++ RemoveDuplicatesFromSortedArray/js/remove_duplicates_other.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var i = 0;
+    for(var j = 1; j < nums.length; j++) {
+        if(nums[j] !== nums[i]) {
+            nums[++i] = nums[j];
+        }
+    }
+    return i + 1;
+};
