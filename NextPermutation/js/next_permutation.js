2d7c9fd7ac9e1b8d5e921ee8838511f08c07a04c
gigi <snailzhao1995@163.com>
2017-09-26 17:33:12 +0800

    add next permutation

---
+++ NextPermutation/js/next_permutation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var nextPermutation = function(nums) {
+    var tmp = 0;
+    for(var i = nums.length - 2; i >= 0; i--) {
+        if(nums[i] >= nums[nums.length - 1]) {
+            nums.push(parseInt(nums.splice(i-1,1).join('')));
+        } else {
+            for(var j = i + 1; j < nums.length; j++){
+                if(nums[j] > nums[i]) {
+                    tmp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = tmp;
+                    i = 0;
+                    break;
+                }
+            }
+        }
+    }
+
+};
