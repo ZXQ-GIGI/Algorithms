f08f6a4bd46a252a343ba0cb97f54cdb4b93d6f1
gigi <snailzhao1995@163.com>
2017-10-11 00:06:06 +0800

    finish

--- CombinationSum/js/combination_sum.js
+++ CombinationSum/js/combination_sum.js
@@ -4,40 +4,32 @@
  * @return {number[][]}
  */
 
- // to finish
 var combinationSum = function(candidates, target) {
     var result = [],
            ele = [];
 
     function dfs(candidates, target){
+
+        if(target == 0) {
+            result.push(ele.slice(0));
+            return;
+        }
+
+        if(target > 0 && candidates.length == 0){
+            return;
+        }
+
         for(var i = 0; i < candidates.length; i++) {
 
-            if(ele.length > 0 && candidates[i] >= ele[ele.length - 1]) {
-                ele.push(candidates[i]);
-            } else {
-                continue;
-            }
+            ele.push(candidates[i]);
             var next_tar = target - candidates[i];
-            var next_can = candidates.filter(function(item, index, array) {
+            var next_can = candidates.slice(i).filter(function(item, index, array) {
                 return (item <= next_tar);
             });
-            //console.log(next_can);
-            if(next_tar == 0) {
-                result.push(ele);
-                return;
-            }
-            if(next_can.length > 0) {
-                dfs(next_can, next_tar);
-                ele.pop();
-                console.log('+1');
-            } else {
-                ele = [];
-                return;
-            }
+            dfs(next_can, next_tar);
+            ele.pop();
         }
     }
     dfs(candidates, target);
     return result;
 };
-
-console.log(combinationSum([2, 3, 6, 7], 7));
